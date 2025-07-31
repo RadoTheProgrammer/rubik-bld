05cf7439620fb9f03a82098841f982c95b0bdfc5
RadoTheProgrammer <rrrado15@gmail.com>
2025-07-31 15:18:36 +0200

    complete change of data, extract functions  functions (get_posf, get_at, remove_solved cubies, input_init_colors)

--- main.py
+++ main.py
@@ -28,32 +28,46 @@ MARK_CYCLE = False
 ALGORITHM = "M2 U M2 U2 M2 U M2"
 
 
-
 EDGES_STICKERS = {
-    "A":((0,0,1),(4,0,1)),
-    "B":((0,1,2),(3,0,1)),
-    "C":((0,2,1),(2,0,1)),
-    "D":((0,1,0),(1,0,1)),
-    "E":((1,0,1),(0,1,0)),
-    "F":((1,1,2),(2,1,0)),
-    "G":((1,2,1),(5,1,0)),
-    "H":((1,1,0),(4,1,2)),
-    "I":((2,0,1),(0,2,1)),
-    "J":((2,1,2),(3,1,0)),
-    "K":((2,2,1),(5,0,1)),
-    "L":((2,1,0),(1,1,2)),
-    "M":((3,0,1),(0,1,2)),
-    "N":((3,1,2),(4,1,0)),
-    "O":((3,2,1),(5,1,2)),
-    "P":((3,1,0),(2,1,2)),
-    "Q":((4,0,1),(0,0,1)),
-    "R":((4,1,2),(1,1,0)),
-    "S":((4,2,1),(5,2,1)),
-    "T":((4,1,0),(3,1,2)),
-    "U":((5,0,1),(2,2,1)),
-    "V":((5,1,2),(3,2,1)),
-    "W":((5,2,1),(4,2,1)),
-    "X":((5,1,0),(1,2,1))
+    "A":(0,4),
+    "B":(0,3),
+    "C":(0,2),
+    "D":(0,1),
+    "E":(1,0),
+    "F":(1,2),
+    "G":(1,5),
+    "H":(1,4),
+    "I":(2,0),
+    "J":(2,3),
+    "K":(2,5),
+    "L":(2,1),
+    "M":(3,0),
+    "N":(3,4),
+    "O":(3,5),
+    "P":(3,2),
+    "Q":(4,0),
+    "R":(4,1),
+    "S":(4,5),
+    "T":(4,3),
+    "U":(5,2),
+    "V":(5,3),
+    "W":(5,4),
+    "X":(5,1)
+}
+
+EDGES_CUBIES = {
+    (0,4): ((0,1),(0,1)),
+    (0,3): ((1,2),(0,1)),
+    (0,2): ((2,1),(0,1)),
+    (0,1): ((1,0),(0,1)),
+    (1,4): ((1,0),(1,2)),
+    (1,2): ((1,2),(1,0)),
+    (2,3): ((1,2),(1,0)),
+    (3,4): ((1,2),(1,0)),
+    (1,5): ((2,1),(1,0)),
+    (2,5): ((2,1),(0,1)),
+    (3,5): ((2,1),(1,2)),
+    (4,5): ((2,1),(2,1))
 }
 
 CORNERS_STICKERS = {
@@ -96,7 +110,7 @@ cube = rubik_impl.Cube.solved(rubik_impl.C_NUMBERS)
 cube.apply(ALGORITHM)
 print(cube)
 
-def get_letters(buffer_colors,stickersdata,func_getc):
+def get_letters(buffer_colors,stickersdata,cubiesdata):
     """
     func_getlp: func to get letter and pos from colors
     func_getc: func to get colors from pos
@@ -107,54 +121,77 @@ def get_letters(buffer_colors,stickersdata,func_getc):
             if letter in all_letters+possible_answers:
                 return letter
             
-    first_cycle = True
-    letters = []
-    stickersdata_rev = {tuple(c[0] for c in value):(key,tuple(c[1:] for c in value)) for key,value in stickersdata.items()}
+    def get_posf(colors):
+        """
+        Get pos on face
+        """
+        colors_sorted = tsort(colors)
+        posf_sorted = cubiesdata[colors_sorted]
+        posf = tuple(posf_sorted[colors_sorted.index(c)] for c in colors)
+        return posf
     
-    cubies = set((tsort(key)) for key in stickersdata_rev.keys()) # type: ignore
-    all_letters = tuple(stickersdata.keys())
-    remaining_cubies = cubies.copy()
-    print(stickersdata_rev)
-    # remove solved cubies
-    buffer_colors_sorted = tsort(buffer_colors)
-    for colors in cubies:
-        letter,pos = stickersdata_rev[colors] # type: ignore
-        colors2 = func_getc(colors,pos)
-        if colors in (colors2,buffer_colors_sorted):
-            remaining_cubies.remove(colors)
-            #print(f"Removed cubie: {colors}")
-    while remaining_cubies:
+    def get_at(colors,posf):
+        return tuple(int(cube.state[color][p[0]][p[1]]) for color,p in zip(colors,posf))
+
+    def remove_solved_cubies():
+        buffer_colors_sorted = tsort(buffer_colors)
+        for colors in cubiesdata:
+            posf = get_posf(colors) # type: ignore # posf = pos on face
+            colors2 = get_at(colors,posf)
+            if colors in (colors2,buffer_colors_sorted):
+                remaining_cubies.remove(colors)
+                print(colors)
+    def input_init_colors():
+        while True:
+            letter = input_letter(".")
+            if letter == ".": # Bro gave up, let's tell possible cubies
+                print("Possible letters: "+" ".join(("/".join(cubies_letters[colors]) for colors in remaining_cubies)))
+                continue
+            init_colors = stickersdata[letter]
+            init_colors_sorted = tsort(init_colors)
+            if init_colors_sorted in remaining_cubies:
+                print("Well chosen")
+                remaining_cubies.remove(init_colors_sorted)
+                return init_colors,init_colors_sorted
+
+            else:
+                print("Incorrect, choose another one")
+
+    def get_init_colors():
         if first_cycle:
             init_colors = buffer_colors
+            init_colors_sorted = tsort(init_colors)
         else:
             if M_PLAN: # ask for init letter
-                while True:
-                    letter = input_letter(".")
-                    if letter == ".": # Bro gave up, let's tell possible cubies
-                        cubies_to_tell = remaining_cubies.copy()
-                        possible_letters = []
-                        for colors,data in stickersdata_rev.items():
-                            colors = tsort(colors)
-                            if colors in cubies_to_tell:
-                                possible_letters.append(data[0])
-                                cubies_to_tell.remove(colors)
-                        print("Possible letters: "+" ".join(possible_letters))
-                        continue
-                    pos0,pos1 = stickersdata[letter]
-
-                    init_colors: tuple[int,int] = tsort((pos0[0],pos1[0])) # type: ignore
-                    if init_colors in remaining_cubies:
-                        print("Well chosen")
-                        break
-                    else:
-                        print("Incorrect, choose another one")
-                remaining_cubies.remove(init_colors)
+                init_colors,init_colors_sorted = input_init_colors()
             else:
-                init_colors = remaining_cubies.pop() # type: ignore # if plan mode, the user choose
+                init_colors = init_colors_sorted = remaining_cubies.pop() # type: ignore # if plan mode, the user choose
+        return init_colors,init_colors_sorted
+    
+    print(get_posf((4,1)))
+    first_cycle = True
+    letters = []
+    stickersdata_rev = {value:key for key,value in stickersdata.items()}
+    cubies_letters = {key:[] for key in cubiesdata}
+    print(cubies_letters)
+    for letter,colors in stickersdata.items():
+        cubies_letters[tsort(colors)].append(letter)
 
+    print(cubies_letters)
+
+    all_letters = tuple(stickersdata)
+    remaining_cubies = list(cubiesdata)
+
+    remove_solved_cubies()
+
+    
+            #print(f"Removed cubie: {colors}")
+    print(remaining_cubies)
+    while remaining_cubies:
+        init_colors,init_colors_sorted = get_init_colors()
 
         current_colors = init_colors
-        init_colors_sorted = tsort(init_colors)
+    
         #print(init_colors)
         first_of_cycle = True
         last_of_cycle = False
@@ -162,7 +199,8 @@ def get_letters(buffer_colors,stickersdata,func_getc):
         while not last_of_cycle: # 1 cycle
             last_of_cycle = not first_of_cycle and tsort(current_colors) == init_colors_sorted
             #print(colors2,last_of_cycle)
-            letter,pos = stickersdata_rev[current_colors]
+            posf = get_posf(current_colors)
+            letter = stickersdata_rev[current_colors]
             #print(f"Letter: {letter}")
             if not (first_cycle and (first_of_cycle or last_of_cycle)):
                 if first_of_cycle and MARK_CYCLE:
@@ -172,9 +210,9 @@ def get_letters(buffer_colors,stickersdata,func_getc):
 
                     #print(f"letter: {letter}")
             if not (first_of_cycle or last_of_cycle):
-                remaining_cubies.remove(tsort(current_colors))
+                remaining_cubies.remove(current_colors)
 
-            current_colors = func_getc(current_colors,pos)
+            current_colors = get_at(current_colors,posf)
             first_of_cycle = False
 
 
@@ -190,12 +228,14 @@ def get_letters(buffer_colors,stickersdata,func_getc):
         pass
     return letters
 
-def getc_edges(colors,pos):
+
+
+def getc_edges(colors,posf):
     return (
-        int(cube.state[colors[0]][pos[0][0]][pos[0][1]]),
-        int(cube.state[colors[1]][pos[1][0]][pos[1][1]])
+        int(cube.state[colors[0]][posf[0][0]][posf[0][1]]),
+        int(cube.state[colors[1]][posf[1][0]][posf[1][1]])
     )
 
 if T_EDGES:
-    print(get_letters((0,3),EDGES_STICKERS,getc_edges))
+    print(get_letters((0,3),EDGES_STICKERS,EDGES_CUBIES))
 
