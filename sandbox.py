00668c5956ec0834d985eacffba5085d33eada58
oravelon <o.ravelon@gmail.com>
2025-08-05 10:51:19 +0200

    analysis mistakes

--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,17 @@ import pandas as pd
 from pandas import Timestamp
 now = pd.Timestamp.now()
 print(repr(now))
-df = pd.read_csv("data_single.csv")
-print(df["PlanMistake"].sum())
-print(df["MemoTime"].sum())
+df = pd.read_csv("data-all.csv")
+dfd = {col:[] for col in ("Number","Avg","Avg5")}
+indexes = ["PlanMistake","MemoMistake","DoMistake"]
+for index in indexes:
+    series = df[index].dropna()
+    dfd["Number"].append(len(series))
+    dfd["Avg"].append(series.mean())
+    series5 = series.iloc[-5:]
+    #print(series5)
+    dfd["Avg5"].append(series5.mean())
+
+df_output = pd.DataFrame(dfd)
+print(df_output)
+
