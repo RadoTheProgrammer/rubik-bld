d245b4c1ce1d75c06bf308542762a029ddb42df2
RadoTheProgrammer <rrrado15@gmail.com>
2025-08-14 12:26:47 +0200

    mistakes-analysis other_cols

--- mistakes-analysis.py
+++ mistakes-analysis.py
@@ -1,13 +1,25 @@
-MISTAKE_NAME = "MemoMistake"
+MODE="MEMO"
+
 import rrstats
 
 import pandas as pd
 from pandas import Timestamp
 now = pd.Timestamp.now()
 
+if MODE=="PLAN":
+    MISTAKE_NAME = "PlanMistake"
+    OTHER_COLS = ["PlanTime"]
+
+elif MODE=="MEMO":
+    MISTAKE_NAME = "MemoMistake"
+    OTHER_COLS = ["MemoTime","MemoRecallTime","MemoEndTime"]
+
+elif MODE=="DO":
+    MISTAKE_NAME = "DoMistake"
+    OTHER_COLS = ["DoTime"]
 df = pd.read_csv("my-data.csv")
-df = df.loc[df[MISTAKE_NAME].notna()][["Datetime",MISTAKE_NAME]]
+df = df.loc[df[MISTAKE_NAME].notna()][["Datetime",MISTAKE_NAME]+OTHER_COLS]
+df["mean5"] = rrstats.StatsSeries(df[MISTAKE_NAME]).myrolling(5).mean()
+
 print(df)
 print(f"Mean: {df[MISTAKE_NAME].mean()}")
-print(f"Mean of 5: {df[MISTAKE_NAME][-5:].mean()}")
-#print(rrstats.StatsSeries(df["PlanMistake"]).myrolling(5).mean())
